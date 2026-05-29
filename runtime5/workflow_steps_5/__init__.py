1f92e78141359ae7a8b391ac515652ae2d824059
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-29 15:33:27 +0200

    Create __init__.py

---
+++ runtime5/workflow_steps_5/__init__.py
@@ -0,0 +1,7 @@
+"""
+Workflow Steps 5.x Package
+Provides deterministic, KG-aware workflow step implementations.
+"""
+
+from .base_step import BaseStep
+from .workflow_step_registry_5 import WorkflowStepRegistry5
