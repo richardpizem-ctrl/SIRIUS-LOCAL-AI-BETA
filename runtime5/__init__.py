fabe1b16735e0626ef76de65b098c05c55316845
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-29 13:24:53 +0200

    Update __init__.py

--- runtime5/__init__.py
+++ runtime5/__init__.py
@@ -2,8 +2,10 @@
 
 from .runtime5 import Runtime5
 from .kg_core import KnowledgeGraph
+from .system_agent_5 import SystemAgent5
 
 __all__ = [
     "Runtime5",
-    "KnowledgeGraph"
+    "KnowledgeGraph",
+    "SystemAgent5",
 ]
