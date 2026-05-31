340c142630c611f1cb31025b3fb997437c26b41c
richardpizem-ctrl <richardpizem@gmail.com>
2026-05-31 06:49:11 +0200

    Create __init__.py

---
+++ src/runtime5/__init__.py
@@ -0,0 +1,28 @@
+"""
+SIRIUS Runtime 5.1.0
+Modulárny Runtime novej generácie
+
+Tento balík obsahuje:
+- Runtime Core 5.1 (hlavný orchestrátor)
+- Repair Entrypoint (spúšťač Self‑Repair Layer 1.0)
+- Health Monitor 5.1
+- Workflow Engine 5.1
+- System Agent 5.1
+- Runtime Config
+
+Runtime5 je navrhnutý ako:
+- modulárny
+- bezpečný
+- opraviteľný
+- auditovateľný
+- kompatibilný so Self‑Repair Layer 1.0 a KG Repair Layer 1.0
+"""
+
+__all__ = [
+    "runtime_core",
+    "repair_entrypoint",
+    "health_monitor",
+    "workflow_engine5",
+    "system_agent5",
+    "runtime_config",
+]
